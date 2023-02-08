8e1e32425085624a6cf5b06ff11b542f31299afd
YashTote <yashtote9999@gmail.com>
2023-02-09 03:48:12 +0530

    Criteria Modal Styling

--- ahp/src/App.js
+++ ahp/src/App.js
@@ -2,14 +2,21 @@ import React, {useState} from 'react';
 import './App.css';
 import Hamburger from './containers/top-tab';
 import Homemain from './containers/homemain';
-import CriteriaModel from './containers/criteriamodal';
+// import CriteriaModel from './containers/criteriamodal';
 
 function App() {
   const[isOpen, setIsOpen] = useState(false);
   return (
     <div>
-      <Hamburger/>
-      {/* <CriteriaModel open = {isOpen}/> */}
+      <div><Hamburger/></div>
+      <div class="button-container">
+      <button onClick = {()=>{setIsOpen(true)}} class="crit-location">
+        Open modal
+       </button>
+      </div>
+      <Homemain open = {isOpen} onClose = {() =>{setIsOpen(false);}}>
+        This is the button
+      </Homemain>
     </div>
   );
 }
--- ahp/src/containers/homemain.js
+++ ahp/src/containers/homemain.js
@@ -3,14 +3,35 @@ import { createPortal } from 'react-dom';
 import './homemain.css'
 
 
-export default function Homemain({open,children,onClose}) {
+export default function Homemain({open,onClose}) {
    if (!open) return null;
     return (
         createPortal
          (<><div  class="overlay"/>
          <div class="matrix">
-            
-           <button onClick={onClose}>X</button> 
+           <table class="crit-info">
+             <tr>
+             <th class="head1">Priopr Basic information</th>
+               <th class="X-column"><button class = "exit-btn" onClick={onClose}>X</button></th>
+             </tr> 
+             <tr>
+           <section class="data-fields">     
+           <form class="form-1">
+            <div>
+             <label for="quantity">Number of alternatives considered:  </label>
+             <input type="number" class="crit-quantity" name="quantity" min="1" max="5"/><br/>
+            </div>
+            <div>
+             <label for="quantity">Number of criteria considered:  </label>
+             <input type="number" class="crit-quantity" name="quantity" min="1" max="5"/><br/>
+             </div>
+             <div class="submit-div">
+              <input class = "submit-btn" type="submit" value="Confirm!" onClick={(e) => {e.preventDefault();}}/>
+             </div>
+           </form>
+           </section>
+           </tr>
+         </table>
          </div>
 
          </>, document.getElementById('portal'))
